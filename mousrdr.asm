56d8ea6a9eddafe893db3555dc5e1c348c7cea86
igrandom <kriz@delogi.be>
2014-12-13 18:00:02 +0100

    mousereader fix

--- mousrdr.asm
+++ mousrdr.asm
@@ -6,21 +6,24 @@ include MOUSRDR.INC
 
 
 ; --- MACROS AND CONSTANTS -----------------------------------------------------
-PUBLIC COL, ROW, VAK
-Other constants	
+
+;Other constants	
 SCREENW		equ 320
 SCREENH		equ 200
-COL 		equ 0 ; in de inc zetten!
-ROW			equ 0 ; in de inc zetten!
-VAK 		equ 0 ; in de inc zetten!
+
 
 ; --- DATA SEGMENT -------------------------------------------------------------
 .DATA        ; data segment, variables
-
+PUBLIC COL
+PUBLIC ROW
+PUBLIC VAK
 oldVideoMode	db ?
 palette     db 0, 0, 0, 13, 53, 56    ; defines black and white
 hardOffset	dw 0 ; test variable
 PLAYFIELD dw 6420
+COL dw 0
+ROW dw 0
+VAK dw 0
 
 ; --- SCREEN BUFFER ------------------------------------------------------------
 .FARDATA?	; segment that contains the screenBuffer for mode 13h drawing
@@ -45,14 +48,17 @@ calcvak PROC FAR
 mov ax, ROW
 sub ax, 20
 mov dx, 0
-div ax, 22
-mul 8
+mov bx, 22
+div bx
+mov bx, 8
+mul bx
 mov ROW, ax
 
-mov ax COL
+mov ax, COL
 sub ax, 20
 mov dx, 0
-div ax, 22
+mov bx, 22
+div bx
 mov bx, ROW
 add ax, bx
 mov VAK, ax
@@ -79,51 +85,46 @@ mousereader PROC FAR
 	push es
 	push cx
 	push ax 
-
 ;-------------------------------
-
-
 ;loop: muisloop, zolang niet geklikt
-
-
-:looped
+loopje:
 mov ax, 03h
 int 33h				;lees uit
 cmp bx, 1
 
 je clicked			 ;if bx = 1 ; if left button is down (1 = left down
-jmp looped
+jmp loopje
 
 
 
 
-:clicked 				;als er geklikt is
-mov row, dx 			; row position
-mov col, cx 			; column position
+clicked: 				;als er geklikt is
+mov ROW, dx 			; row position
+mov COL, cx 			; column position
 
 mov ax, ROW				;kijk of row boven 20 ligt
 cmp ax, 20
 ja comparerowbelow
-jmp looped
+jmp loopje
 
-:comparerowbelow
+comparerowbelow:
 cmp ax, 196				;kijk of row onder 196 ligt
 jb comparecolabove
-jmp looped
+jmp loopje
 
-:comparecolabove
+comparecolabove:
 mov ax, COL				;kijk of col boven 20 ligt
 cmp ax, 20
 ja comparecolbelow
-jmp looped
+jmp loopje
 
-:comparecolbelow
+comparecolbelow:
 cmp ax, 20				;kijk of col onder 196 ligt
 jb calculatevak
-jmp looped
+jmp loopje
 
 
-:calculatevak
+calculatevak:
 call calcvak			;dan is de pijl in het veld : bereken het vakje
 
 ; nu moet er nog gekeken worden of de vakjes clickable zijn
