b089e0ab6b1676954015d1454121d33e2ef6cf0b
igrandom <kriz@delogi.be>
2014-12-13 16:22:30 +0100

    drawbw inc and header

--- drawbw.asm
+++ drawbw.asm
@@ -1,4 +1,35 @@
-      
+    
+    .MODEL large	; multiple data segments and multiple code segments
+.STACK 2048
+
+include DRAWBW.INC
+include PRINTG.INC
+
+
+; --- MACROS AND CONSTANTS -----------------------------------------------------
+PUBLIC COL, ROW, VAK
+Other constants	
+SCREENW		equ 320
+SCREENH		equ 200
+
+; --- DATA SEGMENT -------------------------------------------------------------
+.DATA        ; data segment, variables
+
+oldVideoMode	db ?
+palette     db 0, 0, 0, 13, 53, 56    ; defines black and white
+hardOffset	dw 0 ; test variable
+PLAYFIELD dw 6420
+
+; --- SCREEN BUFFER ------------------------------------------------------------
+.FARDATA?	; segment that contains the screenBuffer for mode 13h drawing
+
+screenBuffer	db 64000 dup(?)	; the 64000 bytes for the screen
+
+
+; ----------------------------- CODE STARTS HERE -------------------------------
+.CODE
+    
+    
     BCoin PROC FAR
     push bp
 	mov bp, sp
@@ -28,7 +59,7 @@
 	pop bx
 	pop bp
 	ret 0
-	bCoin ENDP
+	BCoin ENDP
 	
 	;------------------------------------------
     
