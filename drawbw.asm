aca210f4a4ba476e9c1dc7f2b8bd93c63d407070
Linny123 <lindadc90@gmail.com>
2014-12-13 09:20:59 +0100

    update black and white
    nu voer je vak nummer in en wordt de juiste locatie voor de muntjes
    berekend.

--- drawbw.asm
+++ drawbw.asm
@@ -1,36 +1,4 @@
-	;==============================================================================
-; Example for buffered drawing in mode 13h.
-; Example showing palette manipulation via port IO.
-; Example for a very generic game-loop.
-; ==============================================================================
-.MODEL large	; multiple data segments and multiple code segments
-.STACK 2048  	; stack
-
-; --- INCLUDES -----------------------------------------------------------------
-
-include DRAWR.INC
-include DRAWC.INC
-
-; --- MACROS AND CONSTANTS -----------------------------------------------------
-
-; Other constants	
-SCREENW		equ 320
-SCREENH		equ 200
-
-; --- DATA SEGMENT -------------------------------------------------------------
-.DATA        ; data segment, variables
-oldVideoMode	db ?
-palette     db 0, 0, 0, 13, 53, 56    ; defines black and white
-
-; --- SCREEN BUFFER ------------------------------------------------------------
-.FARDATA?	; segment that contains the screenBuffer for mode 13h drawing
-
-screenBuffer	db 64000 dup(?)	; the 64000 bytes for the screen
-
-
-; ----------------------------- CODE STARTS HERE -------------------------------
-.CODE  
-    
+      
     BCoin PROC FAR
     push bp
 	mov bp, sp
@@ -42,6 +10,9 @@ screenBuffer	db 64000 dup(?)	; the 64000 bytes for the screen
 	;zet waarde op stack
 	
 		mov ax, [bp+4][0]
+		push ax
+	call calcposition
+	
     mov [thisDrawing + 0], ah
     mov [thisDrawing + 1], al
 							;zet kleur in thisDrawing
@@ -72,6 +43,9 @@ screenBuffer	db 64000 dup(?)	; the 64000 bytes for the screen
 	;zet waarde op stack
 	
 		mov ax, [bp+4][0]
+		push ax
+	call calcposition
+	
     mov [thisDrawing + 0], ah
     mov [thisDrawing + 1], al
 							;zet kleur in thisDrawing
